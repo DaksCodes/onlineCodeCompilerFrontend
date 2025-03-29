bbda9abfbc69bd7098eabf2507b67e4d9a3a986e
Dakshita Agrawal <145870184+DaksCodes@users.noreply.github.com>
2025-03-29 15:42:21 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import "./App.css";
 
-const API_URL = "https://onlinecodeeditor-i88n.onrender.com/";
+const API_URL = "https://onlinecodeeditor-i88n.onrender.com/run";
 
 function App() {
   const [darkMode, setDarkMode] = useState(false);
